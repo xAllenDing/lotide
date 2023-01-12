b5d66d320bfd35f593eb97e63b523ffec291426b
Allen Ding <allen.ding@hotmail.com>
2023-01-11 16:44:30 -0800

    created a function that takes in a sentence and counts the letters and returns an object of the count

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⚠️⚠️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function (sentence) {
+  const results = {};
+  for (const letters of sentence) {
+    if (letters === " ") {
+      continue;
+    }
+    // checks if the key has been made
+    if (results[letters]) {
+      results[letters] = ++results[letters];
+    } else {
+      //creates the key for the letter and gives it a value of 1
+      results[letters] = 1;
+    }
+  }
+  return results;
+}
+
+console.log(countLetters("lighthouse in the house"));
