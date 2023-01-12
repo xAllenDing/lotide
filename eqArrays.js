54a276eec065bb405b62f04bf9c7b7c5c45dd6c0
Allen Ding <allen.ding@hotmail.com>
2023-01-11 21:06:19 -0800

    updated

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`⚠️⚠️ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 let eqArrays = function (arr1, arr2) {
   if(arr1.length !== arr2.length){
     return false;
@@ -17,7 +9,16 @@ let eqArrays = function (arr1, arr2) {
     }
   }
   return true;
-}
+};
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⚠️⚠️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
