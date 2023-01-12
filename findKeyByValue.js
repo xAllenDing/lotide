0b5130c4a8912704dacecf2343f98ffa2b144ef2
Allen Ding <allen.ding@hotmail.com>
2023-01-11 19:52:47 -0800

    created function to match the key with a given value and given object

---
+++ findKeyByValue.js
@@ -0,0 +1,32 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⚠️⚠️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function (obj, value) {
+  for (let key in obj) {
+    let keyValue = obj[key];
+    if (keyValue === value) {
+      return key;
+    } 
+  }
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+console.log(findKeyByValue(bestTVShowsByGenre, "woof"));
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "woof");
+console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse"));
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), 1);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), 1);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
