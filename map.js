c258cf248240ee4bebf47d158836022933877332
Allen Ding <allen.ding@hotmail.com>
2023-01-12 20:00:23 -0800

    created our own map function

---
+++ map.js
@@ -0,0 +1,38 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback){
+  const results = [];
+  for(let item of array){
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+let eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+let assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`⚠️⚠️ Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+}
+
+
+assertArraysEqual(map(words, word => word[1]), ['r', 'o', 'o', 'a', 'o']);
+assertArraysEqual(map(words, word => word[1]), ['g', 'c', 't', 'm', 't']);
+assertArraysEqual(map(words, word => word[0]), ['g', 'c', 't', 'm', 't']);
