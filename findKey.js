4f9192fa9e1d5df783b4b24d1772c00558255e98
Allen Ding <allen.ding@hotmail.com>
2023-01-12 21:39:16 -0800

    created function that returns the key of the given callback function truth value

---
+++ findKey.js
@@ -0,0 +1,52 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⚠️⚠️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function (object, callback) {
+  for (let key in object) {
+    let checkBool = callback(object[key]);
+    if (checkBool) {
+      return key;
+    }
+  }
+}
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2));
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2), "noma");
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 0), undefined);
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2), "Blue Hill");
