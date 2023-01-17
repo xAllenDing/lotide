b7edc309f78da98088bfcfdf1dfb7d6d64028e14
Allen Ding <allen.ding@hotmail.com>
2023-01-16 19:07:22 -0800

    required assertEqual and head to run tests

--- test/headTest.js
+++ test/headTest.js
@@ -2,7 +2,7 @@ const assertEqual = require('../assertEqual');
 const head = require('../head');
 
 // Test cases
-(assertEqual(head([5,6,7]), 5));
+assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head(["hi"]), "hi");
 assertEqual(head([]), 5);
