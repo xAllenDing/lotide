5f77738d39ab6a3e1ef5ce1e3afc601f7f44cbac
Allen Ding <allen.ding@hotmail.com>
2023-01-16 18:49:28 -0800

    refactored eqArrays so that its test required assertEqual to run

---
+++ test/eqArraysTest.js
@@ -0,0 +1,10 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+
+//test code
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArrays(["1", "2"], ["1", "2", 3]), false); 
+assertEqual(eqArrays([1, 2, 3], ["1", "2", "3"]), false);
